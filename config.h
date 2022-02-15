023832a66c99e15c93870e5df90381a208995c72
keijo0 <keijo0@github.com>
2022-02-15 18:06:34 +0200

    added patch so new tabs go right

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "mono:size=11", "fontawesome:size=11" };
-static const char dmenufont[]       = "mono:size=11";
+static const char *fonts[]          = { "monospace:size=11", "fontawesome:size=11" };
+static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#999999";
 static const char col_gray3[]       = "#eeeeee";
@@ -36,6 +36,7 @@ static const Rule rules[] = {
         { "Steam",    	NULL,       NULL,	1 << 6,       0,           -1 },
         { "Virt-manager",  NULL,    NULL,	1 << 4,       0,           -1 },
         { "KeePassXC",  NULL,       NULL,	1 << 2,       0,           -1 },
+	{ "Thunderbird",  NULL,     NULL,       1 << 3,       0,           -1 },
 
 
 };
