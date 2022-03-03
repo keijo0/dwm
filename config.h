bc1091524653d596a7384034a8cfa9af3d65b339
keijo0 <keijo0@github.com>
2022-03-03 04:47:43 +0200

    bla

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Jetbrains Mono:size=11", "fontawesome:size=11" };
-static const char dmenufont[]       = "Jetbrains Mono:size=11";
+static const char *fonts[]          = { "Bitstream Vera Sans Mono:size=11", "fontawesome:size=11" };
+static const char dmenufont[]       = "Bitstream Vera Sans Mono:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#999999";
 static const char col_gray3[]       = "#eeeeee";
@@ -26,16 +26,19 @@ static const char *colors[][3]      = {
 
 /* tagging */
 //static const char *tags[] = { "", "", "", "", "", "", "", "", "", "" };
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"/*"11", "12", "13", "14", "15", "16", "17", "18", "19", "20"*/ };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
-        { "Firefox",    NULL,       NULL,	1 << 8,       0,           -1 },
-        { "discord",    NULL,       NULL,	1 << 7,       0,           -1 },
-        { "Steam",    	NULL,       NULL,	1 << 6,       0,           -1 },
-        { "Virt-manager",  NULL,    NULL,	1 << 4,       0,           -1 },
-        { "KeePassXC",  NULL,       NULL,	1 << 2,       0,           -1 },
-	{ "Thunderbird",  NULL,     NULL,       1 << 3,       0,           -1 },
+        { "Firefox",     NULL,      NULL,	1 << 8,       0,           -1 },
+        { "chromium",    NULL,      NULL,	1 << 8,       0,           -1 },
+        { "discord",     NULL,      NULL,	1 << 7,       0,           -1 },
+        { "Steam",       NULL,      NULL,	1 << 6,       0,           -1 },
+        { "plutonium",   NULL, 	    NULL,   	1 << 5,       0,           -1 },
+        { "Virt-manager", NULL,     NULL,	1 << 4,       0,           -1 },
+        { "KeePassXC",   NULL,      NULL,	1 << 2,       0,           -1 },
+	{ "Thunderbird", NULL,      NULL,       1 << 3,       0,           -1 },
+        { "spotify",     NULL,      NULL,	1 << 9,       0,           -1 },
 
 };
 
@@ -67,6 +70,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray3, "-sb", col_purple, "-sf", col_black,  NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
+static const char *crmcmd[] = { "chromium", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
 static const char *musicmd[]  = { "spotify", NULL };
 static const char *dcmd[]  = { "discord", NULL };
@@ -86,8 +90,9 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
-        { MODKEY,                       XK_m,      spawn,          {.v = browsercmd } },
-        { MODKEY,                       XK_n,	   spawn,          {.v = dcmd } },
+        { MODKEY,                       XK_n,      spawn,          {.v = browsercmd } },
+        { MODKEY,                       XK_m,	   spawn,          {.v = crmcmd } },
+        { MODKEY,                       XK_c,	   spawn,          {.v = dcmd } },
         { MODKEY,                       XK_b,	   spawn,          {.v = filecmd } },
         { MODKEY,                       XK_v,	   spawn,          {.v = pavucmd } },
         { MODKEY,                       XK_F5,      spawn,          {.v = musicmd } },
