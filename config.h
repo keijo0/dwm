4fa9f164faeb617d1ec8908e77ec0b0b1dbc808a
keijo0 <keijo0@github.com>
2022-04-04 20:16:55 +0300

    changes

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gapppx    = 8; 	/* gaps */
-static const unsigned int snap      = 0;       /* snap pixel */
+static const unsigned int gapppx    = 10; 	/* gaps */
+static const unsigned int snap      = 25;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayonleft = 1;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 0;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Bitstream Vera Sans Mono:size=9", "fontawesome:size=9" };
-static const char dmenufont[]       = "Bitstream Vera Sans Mono:size=9";
+static const char *fonts[]          = { "Quicksand:size=9", "fontawesome:size=9" };
+static const char dmenufont[]       = "Quicksand:size=9";
 
 /* colors */
 static const char col_gray1[]       = "#222222";
@@ -34,12 +34,7 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 //        { "chromium",    NULL,      NULL,	1 << 8,       0,           -1 },
-//        { "discord",     NULL,      NULL,	1 << 7,       0,           -1 },
         { "Steam",       NULL,      NULL,	1 << 8,       0,           -1 },
-//        { "plutonium",   NULL, 	    NULL,   	1 << 5,       0,           -1 },
-//        { "Virt-manager", NULL,     NULL,	1 << 4,       0,           -1 },
-//        { "KeePassXC",   NULL,      NULL,	1 << 2,       0,           -1 },
-//	{ "Thunderbird", NULL,      NULL,       1 << 3,       0,           -1 },
 }; 
 
 
@@ -51,8 +46,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-        { "[T]",      tile },    /* first entry is default */
+//        { "[T]",      tile },    /* first entry is default */
         { "[F]",      NULL },    /* no layout function means floating behavior */
+        { "[T]",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
 };
 
@@ -72,9 +68,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray3, "-sb", col_purple, "-sf", col_black,  NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 static const char *crmcmd[] = { "chromium", NULL };
-static const char *browsercmd[]  = { "firefox", NULL };
 static const char *musicmd[]  = { "spotify", NULL };
-static const char *dcmd[]  = { "discord", NULL };
 static const char *filecmd[]  = { "pcmanfm", NULL };
 static const char *pavucmd[]  = { "pavucontrol", NULL };
 
@@ -91,17 +85,15 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
-        { MODKEY,                       XK_n,      spawn,          {.v = browsercmd } },
         { MODKEY,                       XK_m,	   spawn,          {.v = crmcmd } },
-        { MODKEY,                       XK_c,	   spawn,          {.v = dcmd } },
         { MODKEY,                       XK_b,	   spawn,          {.v = filecmd } },
         { MODKEY,                       XK_v,	   spawn,          {.v = pavucmd } },
         { MODKEY,                       XK_F5,      spawn,          {.v = musicmd } },
 	{ MODKEY,                       XK_o,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_F3,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_F4,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
@@ -120,8 +112,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-        { MODKEY,                       XK_minus,  setgaps,        {.i = -2 } },
-        { MODKEY,                       XK_equal,  setgaps,        {.i = +2 } },
+        { MODKEY,                       XK_minus,  setgaps,        {.i = -4 } },
+        { MODKEY,                       XK_equal,  setgaps,        {.i = +4 } },
         { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
  	{ MODKEY,                       XK_F12,     spawn,          {.v = upvol   } },
  	{ MODKEY,                       XK_F11,     spawn,          {.v = downvol } },
