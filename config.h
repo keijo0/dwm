f00c697f5562cd9fb9595fea9bb81110d673ea9a
idiot <95128521+keijo0@users.noreply.github.com>
2022-06-19 23:11:06 +0300

    Add files via upload

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gapppx    = 10; 	/* gaps */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gapppx    = 5; 	/* gaps */
 static const unsigned int snap      = 15;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 1;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 0;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int showsystray        = 0;     /* 0 means no systray */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "IBM Plex mono:size=8", "fontawesome:size=8" };
-static const char dmenufont[]       = "IBM Plex mono:size=8";
+static const char *fonts[]          = { "Cousine:size=8", "fontawesome:size=8" };
+static const char dmenufont[]       = "Cousine:size=8";
 
 /* colors */
 static const char col_gray1[]       = "#222222";
@@ -18,12 +18,13 @@ static const char col_gray2[]       = "#999999";
 static const char col_gray3[]       = "#eeeeee";
 static const char col_gray4[]       = "#dddddd";
 static const char col_cyan[]        = "#005577";
-static const char col_black[]	    = "#8987d1";
-static const char col_purple[]	    = "#1f1f1f";
+static const char col_purple[]	    = "#8987d1";
+static const char col_black1[]      = "#000000";
+static const char col_black[]	    = "#1f1f1f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_purple, col_black, col_gray1 },
-	[SchemeSel]  = { col_black, col_purple, col_black },
+	[SchemeNorm] = { col_purple, col_black1, col_gray1 },
+	[SchemeSel]  = { col_gray2, col_gray1, col_gray1 },
 };
 
 /* tagging */
@@ -31,7 +32,7 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" 
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
-        { "Steam",       NULL,      NULL,	1 << 8,       0,           -1 },
+        { "Steam",       NULL,      NULL,	1 << 8,       1,           -1 },
 }; 
 
 
@@ -43,9 +44,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-        { "T",      tile },    /* first entry is default */
-	{ "F",      NULL },    /* no layout function means floating behavior */
-	{ "M",      monocle },
+	{ "[Floating]",      NULL },    /* no layout function means floating behavior */
+        { "[Tiling]",      tile },    /* first entry is default */
+	{ "[Monocle]",      monocle },
 };
 
 /* key definitions */
@@ -56,17 +57,20 @@ static const Layout layouts[] = {
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/bash", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/zsh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_purple, "-nf", col_black, "-sb", col_purple, "-sf", col_black,  NULL };
-static const char *termcmd[]  = { "urxvt", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_purple, "-sb", col_black, "-sf", col_purple,  NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *crmcmd[] = { "chromium", NULL };
 static const char *vrmcmd[] = { "pavucontrol", NULL };
-static const char *sptcmd[] = { "spotify", NULL };
 static const char *bcmd[] = { "pcmanfm", NULL };
+static const char *ncmd[] = { "nvidia-settings", NULL };
+static const char *scmd[] = { "spotify", NULL };
+static const char *pcmd[] = { "blueman-manager", NULL };
+static const char *dcmd[] = { "discord", NULL };
+static const char *tcmd[] = { "twitch", NULL };
 
 
 /* volume things */
@@ -85,7 +89,11 @@ static Key keys[] = {
         { MODKEY,                       XK_m,	   spawn,          {.v = crmcmd } },
         { MODKEY,                       XK_v,      spawn,          {.v = vrmcmd } },
         { MODKEY,                       XK_b,	   spawn,          {.v = bcmd } },
-	{ MODKEY,			XK_F5,	   spawn,	   {.v = sptcmd } },
+        { MODKEY,                       XK_c,	   spawn,          {.v = ncmd } },
+        { MODKEY,                       XK_x,     spawn,          {.v = pcmd } },
+        { MODKEY,                       XK_z,     spawn,          {.v = dcmd } },
+        { MODKEY,                       XK_n,     spawn,          {.v = tcmd } },
+        { MODKEY,                       XK_F5,     spawn,          {.v = scmd } },
 	{ MODKEY,                       XK_o,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -109,8 +117,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-        { MODKEY,                       XK_minus,  setgaps,        {.i = -4 } },
-        { MODKEY,                       XK_equal,  setgaps,        {.i = +4 } },
+        { MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
+        { MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
         { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
  	{ MODKEY,                       XK_F12,     spawn,          {.v = upvol   } },
  	{ MODKEY,                       XK_F11,     spawn,          {.v = downvol } },
@@ -130,13 +138,11 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
 };
 
-/* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
